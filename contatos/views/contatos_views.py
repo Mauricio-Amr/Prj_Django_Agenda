3ad02e922fc3cf6c22d105806490987e568c6637
Mauricio Amaral <mauricio.s.amr@gmail.com>
2024-10-26 13:29:07 -0300

    Inserido paginação

--- contatos/views/contatos_views.py
+++ contatos/views/contatos_views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contatos.models import Contato
 from django.db.models import Q
+
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 
 def index(request):
-    contatos = Contato.objects.filter(show=True).order_by('-id')[0:10]
+    contatos = Contato.objects.filter(show=True).order_by('-id')
 
-    context = {'contatos': contatos,
+    paginator = Paginator(contatos, 10)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,
                'site_title': 'Contato - ',
                }
 
@@ -37,8 +45,14 @@ def search(request):
 
     # print(contatos.query)
 
-    context = {'contatos': contatos,
+    paginator = Paginator(contatos, 10)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,
                'site_title': 'Search - ',
+               'search_value': search_value,
                }
 
     return render(
